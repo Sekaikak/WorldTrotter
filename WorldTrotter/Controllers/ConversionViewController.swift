d4632c84ece21b1360245fa9a067ecb1288e386b
Kai <goosyei@gmail.com>
2020-05-30 12:20:51 -0700

    Chapter 6 1

--- WorldTrotter/Controllers/ConversionViewController.swift
+++ WorldTrotter/Controllers/ConversionViewController.swift
@@ -9,43 +9,7 @@
 import UIKit
 
 class ConversionViewController: UIViewController {
-    override func viewDidLoad() {
-        view.backgroundColor = UIColor.systemBackground
-        let fahLabel = UILabel()
-        fahLabel.text = "212"
-        fahLabel.font = fahLabel.font.withSize(70)
-        fahLabel.textColor = UIColor(named: "burntOrange")
-        
-        let secondLine = UILabel()
-        secondLine.text = "degrees Fahrenheit"
-        secondLine.textColor = UIColor(named: "burntOrange")
-        secondLine.font = secondLine.font.withSize(36)
-        
-        let thirdLine = UILabel()
-        thirdLine.text = "is really"
-        thirdLine.font = thirdLine.font.withSize(36)
-        
-        let celLabel = UILabel()
-        celLabel.text = "100"
-        celLabel.font = celLabel.font.withSize(70)
-        celLabel.textColor = UIColor(named: "burntOrange")
-        
-        let lastLine = UILabel()
-        lastLine.text = "degrees Celsius"
-        lastLine.textColor = UIColor(named: "burntOrange")
-        lastLine.font = lastLine.font.withSize(36)
-        
-        constraintCustom(for: fahLabel, from: view.safeAreaLayoutGuide.topAnchor)
-        constraintCustom(for: secondLine, from: fahLabel.bottomAnchor)
-        constraintCustom(for: thirdLine, from: secondLine.bottomAnchor)
-        constraintCustom(for: celLabel, from: thirdLine.bottomAnchor)
-        constraintCustom(for: lastLine, from: celLabel.bottomAnchor)
-    }
-    func constraintCustom(for labelItSelf: UILabel, from labelConsFrom: NSLayoutAnchor<NSLayoutYAxisAnchor>) {
-        labelItSelf.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(labelItSelf)
-        labelItSelf.topAnchor.constraint(lessThanOrEqualTo: labelConsFrom, constant: 8).isActive = true
-        labelItSelf.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    }
+    
+    
 }
 
